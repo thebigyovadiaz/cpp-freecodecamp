e541fe3d6c011c2183fbe2c4fd00be1e714b7d18
YovaDiaz <thebigyovadiaz@gmail.com>
2025-10-09 20:27:42 -0300

    Chapter 10: Building guessing game

--- src/main.cpp
+++ src/main.cpp
@@ -2,43 +2,34 @@
 // main.cpp
 // FreeCodeCamp-Course
 //
-// Chapter 3: working with strings
+// Chapter 10: building a guessing game
 
 #include <iostream>
 
-using namespace std;
-
 int main() {
-    string phrase = "FreeCodeCamp Course";
-    cout << "Phrase: " << phrase << endl;
+    int secretNum = 7;
+    int guessNum = 0;
+    int guessCount = 0;
+    int guessLimit = 3;
+    bool outOfGuesses = false;
     
-    // Copy phrase
-    string copyPhrase = phrase;
+    while(secretNum != guessNum && !outOfGuesses) {
+        if (guessCount < guessLimit) {
+            std::cout << "Enter number: ";
+            std::cin >> guessNum;
+            guessCount++;
+        } else {
+            outOfGuesses = true;
+        }
+    }
     
-    // Update char in original phrase
-    phrase[0] = 'T';
-    cout << "Phrase updated: " << phrase << endl;
+    if (outOfGuesses) {
+        std::cout << "\nYou Lose!";
+    } else {
+        std::cout << "\nY2ou Win!";
+    }
     
-    // Get length original phrase
-    long lengthPhO = phrase.length();
-    cout << "Length: " << lengthPhO << endl;
-    
-    // Find word in copy phrase
-    cout << "Copy phrase: " << copyPhrase << endl;
-    string word = "Course";
-    long startWordIndex = copyPhrase.find(word);
-    cout << "Word start index: " << startWordIndex << endl;
-    
-    // Replace word in copy phrase
-    long lengthWord = word.length();
-    copyPhrase.replace(startWordIndex, lengthWord, "");
-    cout << "Phrase with word replaced: " << copyPhrase << endl;
-    
-    // Substring of copy phrase
-    string subStringPhrase = copyPhrase.substr(0, 4);
-    cout << "New Substring: " << subStringPhrase;
-    
-    cout << "\n\n";
+    std::cout << "\n\n";
     
     return EXIT_SUCCESS;
 }
