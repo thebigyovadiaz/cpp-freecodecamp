7a6acaf2d56d8a4bc54d1b6fa1564d1a0316cf24
YovaDiaz <thebigyovadiaz@gmail.com>
2025-10-09 22:01:07 -0300

    Chapter 11: working with for loops

--- src/main.cpp
+++ src/main.cpp
@@ -2,43 +2,33 @@
 // main.cpp
 // FreeCodeCamp-Course
 //
-// Chapter 3: working with strings
+// Chapter 11: working with for loops
 
 #include <iostream>
 
-using namespace std;
+int getPow(int baseNum, int powNum) {
+	int result = 1;
+	
+	for (int i = 0; i < powNum; i++) {
+		result = result * baseNum;
+	}
+	
+	return result;
+}
 
 int main() {
-    string phrase = "FreeCodeCamp Course";
-    cout << "Phrase: " << phrase << endl;
+	int nums [] = {1, 23, 4, 7, 100};
+	
+	// Use sizeof to get array length
+	int numsLength = sizeof(nums) / sizeof(nums[0]);
+	
+	for(int numb = 0; numb < numsLength; numb++) {
+		std::cout << nums[numb] << std::endl;
+	}
+	
+	std::cout << "\nResult GetPow: " << getPow(2, 4);
     
-    // Copy phrase
-    string copyPhrase = phrase;
-    
-    // Update char in original phrase
-    phrase[0] = 'T';
-    cout << "Phrase updated: " << phrase << endl;
-    
-    // Get length original phrase
-    long lengthPhO = phrase.length();
-    cout << "Length: " << lengthPhO << endl;
-    
-    // Find word in copy phrase
-    cout << "Copy phrase: " << copyPhrase << endl;
-    string word = "Course";
-    long startWordIndex = copyPhrase.find(word);
-    cout << "Word start index: " << startWordIndex << endl;
-    
-    // Replace word in copy phrase
-    long lengthWord = word.length();
-    copyPhrase.replace(startWordIndex, lengthWord, "");
-    cout << "Phrase with word replaced: " << copyPhrase << endl;
-    
-    // Substring of copy phrase
-    string subStringPhrase = copyPhrase.substr(0, 4);
-    cout << "New Substring: " << subStringPhrase;
-    
-    cout << "\n\n";
+    std::cout << "\n\n";
     
     return EXIT_SUCCESS;
 }
